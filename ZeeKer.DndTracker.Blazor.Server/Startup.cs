e300267a22ecc539c10674c9210db93f0e06cf1d
Alex Shibalkin <shibalkun@ya.ru>
2024-05-20 14:10:58 +0500

    Рефакторинг Startup файла

--- ZeeKer.DndTracker.Blazor.Server/Startup.cs
+++ ZeeKer.DndTracker.Blazor.Server/Startup.cs
@@ -2,7 +2,6 @@
 using DevExpress.ExpressApp.ApplicationBuilder;
 using DevExpress.ExpressApp.Blazor.ApplicationBuilder;
 using DevExpress.ExpressApp.Blazor.Services;
-using DevExpress.Persistent.Base;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components.Server.Circuits;
 using Microsoft.EntityFrameworkCore;
@@ -18,8 +17,6 @@ public class Startup {
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services) {
         services.AddSingleton(typeof(Microsoft.AspNetCore.SignalR.HubConnectionHandler<>), typeof(ProxyHubConnectionHandler<>));
 
@@ -47,28 +44,13 @@ public class Startup {
                 })
                 .AddValidation()
                 .AddViewVariants()
-                .Add<ZeeKer.DndTracker.Module.DndTrackerModule>()
+                .Add<Module.DndTrackerModule>()
             	.Add<DndTrackerBlazorModule>();
             builder.ObjectSpaceProviders
                 .AddSecuredEFCore(options => options.PreFetchReferenceProperties())
-                    .WithDbContext<ZeeKer.DndTracker.Module.BusinessObjects.DndTrackerEFCoreDbContext>((serviceProvider, options) => {
-                        // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
-                        // Do not use this code in production environment to avoid data loss.
-                        // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
-                        //options.UseInMemoryDatabase("InMemory");
-                        string connectionString = null;
-#if RELEASE
-                        
-                        if(Configuration.GetConnectionString("ConnectionStringRelease") != null) {
-                            connectionString = Configuration.GetConnectionString("ConnectionStringRelease");
-                        }
-#else
+                    .WithDbContext<Module.BusinessObjects.DndTrackerEFCoreDbContext>((serviceProvider, options) => {
+                        var connectionString = GetConnectionString();
 
-                        if (Configuration.GetConnectionString("ConnectionString") != null) {
-                            connectionString = Configuration.GetConnectionString("ConnectionString");
-                        }
-#endif
-                        ArgumentNullException.ThrowIfNull(connectionString);
                         options.UseSqlServer(
                             connectionString,
                             sqlOptions => 
@@ -86,22 +68,12 @@ public class Startup {
             builder.Security
                 .UseIntegratedMode(options => {
                     options.Lockout.Enabled = true;
-
-                    options.RoleType = typeof(PermissionPolicyRole);
-                    // ApplicationUser descends from PermissionPolicyUser and supports the OAuth authentication. For more information, refer to the following topic: https://docs.devexpress.com/eXpressAppFramework/402197
-                    // If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
-                    options.UserType = typeof(ZeeKer.DndTracker.Module.BusinessObjects.ApplicationUser);
-                    // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
-                    // If you use PermissionPolicyUser or a custom user type, comment out the following line:
-                    options.UserLoginInfoType = typeof(ZeeKer.DndTracker.Module.BusinessObjects.ApplicationUserLoginInfo);
+                    options.RoleType = typeof(PermissionPolicyRole); 
+                    options.UserType = typeof(Module.BusinessObjects.ApplicationUser);
+                    options.UserLoginInfoType = typeof(Module.BusinessObjects.ApplicationUserLoginInfo);
                     options.Events.OnSecurityStrategyCreated += securityStrategy => {
-                        // Use the 'PermissionsReloadMode.NoCache' option to load the most recent permissions from the database once
-                        // for every DbContext instance when secured data is accessed through this instance for the first time.
-                        // Use the 'PermissionsReloadMode.CacheOnFirstAccess' option to reduce the number of database queries.
-                        // In this case, permission requests are loaded and cached when secured data is accessed for the first time
-                        // and used until the current user logs out. 
-                        // See the following article for more details: https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Security.SecurityStrategy.PermissionsReloadMode.
-                        ((SecurityStrategy)securityStrategy).PermissionsReloadMode = PermissionsReloadMode.CacheOnFirstAccess;
+                        var strategy = (SecurityStrategy)securityStrategy;
+                        strategy.PermissionsReloadMode = PermissionsReloadMode.CacheOnFirstAccess;
                     };
                 })
                 .AddPasswordAuthentication(options => {
@@ -113,7 +85,7 @@ public class Startup {
         });
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
         
         if (env.IsDevelopment()) {
@@ -144,5 +116,26 @@ public class Startup {
             endpoints.MapFallbackToPage("/_Host");
             endpoints.MapControllers();
         });
+    }
+
+
+    private string GetConnectionString()
+    {
+        string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+#if RELEASE
+
+        if (String.IsNullOrEmpty(connectionString) &&
+        Configuration.GetConnectionString("ConnectionStringRelease") is not null)
+            connectionString = Configuration.GetConnectionString("ConnectionStringRelease");
+
+#else
+
+        if (Configuration.GetConnectionString("ConnectionString") is not null)
+            connectionString = Configuration.GetConnectionString("ConnectionString");
+                        
+#endif
+        ArgumentNullException.ThrowIfNull(connectionString);
+
+        return connectionString;
     }
 }
