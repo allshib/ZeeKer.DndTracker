5653c5b5698b55b982a264fa1ba932e5b5ef0e5f
Alex Shibalkin <shibalkun@ya.ru>
2024-05-30 17:04:57 +0500

    Добавлена возможность редактировать условие доступности черты #SCRUM-9

--- ZeeKer.DndTracker.Module/BusinessObjects/Feat.cs
+++ ZeeKer.DndTracker.Module/BusinessObjects/Feat.cs
@@ -1,15 +1,18 @@
 using DevExpress.Data.Filtering;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl.EF;
 using DevExpress.Persistent.Validation;
+using DevExpress.XtraPrinting.Native;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -33,8 +36,12 @@ namespace ZeeKer.DndTracker.Module.BusinessObjects
         [XafDisplayName("Описание")]
         public virtual string Description { get; set; }
 
+        [Browsable(false), NotMapped]
+        public virtual Type CharacterType => typeof(Character);
+
         [StringLength(700)]
-        [XafDisplayName("Условие")]
+        [XafDisplayName("Условие"), CriteriaOptions(nameof(CharacterType))]
+        [EditorAlias(EditorAliases.PopupCriteriaPropertyEditor)]
         public virtual string Condition { get; set; }
 
         [XafDisplayName("Бонусы"), Aggregated]
