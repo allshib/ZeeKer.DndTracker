3fb0a8a9cd899ee0699c5809ecd682f706905f7c
Alex Shibalkin <shibalkun@ya.ru>
2024-06-03 20:18:19 +0500

    Обновление ManageCoinsController: упрощение создания SimpleAction
    В файле `ManageCoinsController.cs` были сделаны следующие изменения:
    - Добавлена функция `CreateSimpleAction`, которая упрощает создание новых действий `SimpleAction`, принимая идентификатор, подпись, категорию и обработчик событий в качестве параметров. #SCRUM-7

--- ZeeKer.DndTracker.Module/Controllers/ManageCoinsController.cs
+++ ZeeKer.DndTracker.Module/Controllers/ManageCoinsController.cs
@@ -34,105 +34,24 @@ namespace ZeeKer.DndTracker.Module.Controllers
             TargetObjectType = typeof(Character);
 
             #region InitActions
-            var addGold1Gold = new SimpleAction(this, "AddOneGold", ActionCategories.AddGoldCategory)
-            {
-                Caption = "+1з"
-            };
-            addGold1Gold.Execute += AddGold1Gold_Execute;
+            CreateSimpleAction("+1з", "AddOneGold", ActionCategories.AddGoldCategory, AddGold1Gold_Execute);
+            CreateSimpleAction("+10з", "AddTenGold", ActionCategories.AddGoldCategory, AddGold10Gold_Execute);
+            CreateSimpleAction("+50з", "Add50Gold", ActionCategories.AddGoldCategory, AddGold50Gold_Execute);
+            CreateSimpleAction("+100з", "Add100Gold", ActionCategories.AddGoldCategory, AddGold100Gold_Execute);
+            CreateSimpleAction("+250з", "Add250Gold", ActionCategories.AddGoldCategory, AddGold250Gold_Execute);
+            CreateSimpleAction("+500з", "Add500Gold", ActionCategories.AddGoldCategory, AddGold500Gold_Execute);
 
+            CreateSimpleAction("-1з", "Remove1Gold", ActionCategories.RemoveGoldCategory, RemoveGold1Gold_Execute);
+            CreateSimpleAction("-2з", "Remove2Gold", ActionCategories.RemoveGoldCategory, RemoveGold2Gold_Execute);
+            CreateSimpleAction("-3з", "Remove3Gold", ActionCategories.RemoveGoldCategory, RemoveGold3Gold_Execute);
+            CreateSimpleAction("-5з", "Remove5Gold", ActionCategories.RemoveGoldCategory, RemoveGold5Gold_Execute);
+            CreateSimpleAction("-10з", "RemoveTenGold", ActionCategories.RemoveGoldCategory, RemoveGold10Gold_Execute);
+            CreateSimpleAction("-100з", "Remove100Gold", ActionCategories.RemoveGoldCategory, RemoveGold100Gold_Execute);
+            CreateSimpleAction("-250з", "Remove250Gold", ActionCategories.RemoveGoldCategory, RemoveGold250Gold_Execute);
+            CreateSimpleAction("-500з", "Remove500Gold", ActionCategories.RemoveGoldCategory, RemoveGold500Gold_Execute);
 
-            var addGold10Gold = new SimpleAction(this, "AddTenGold", ActionCategories.AddGoldCategory)
-            {
-                Caption = "+10з"
-            };
-            addGold10Gold.Execute += AddGold10Gold_Execute;
-
-            var addGold50Gold = new SimpleAction(this, "Add50Gold", ActionCategories.AddGoldCategory)
-            {
-                Caption = "+50з"
-            };
-            addGold50Gold.Execute += AddGold50Gold_Execute;
-
-            var addGold100Gold = new SimpleAction(this, "Add100Gold", ActionCategories.AddGoldCategory)
-            {
-                Caption = "+100з"
-            };
-            addGold100Gold.Execute += AddGold100Gold_Execute;
-
-            var addGold250Gold = new SimpleAction(this, "Add250Gold", ActionCategories.AddGoldCategory)
-            {
-                Caption = "+250з"
-            };
-            addGold250Gold.Execute += AddGold250Gold_Execute;
-
-            var addGold500Gold = new SimpleAction(this, "Add500Gold", ActionCategories.AddGoldCategory)
-            {
-                Caption = "+500з"
-            };
-            addGold500Gold.Execute += AddGold500Gold_Execute;
-
-
-
-            var removeGold1Gold = new SimpleAction(this, "Remove1Gold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-1з"
-            };
-            removeGold1Gold.Execute += RemoveGold1Gold_Execute;
-
-            var removeGold2Gold = new SimpleAction(this, "Remove2Gold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-2з"
-            };
-            removeGold2Gold.Execute += RemoveGold2Gold_Execute;
-
-            var removeGold3Gold = new SimpleAction(this, "Remove3Gold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-3з"
-            };
-            removeGold3Gold.Execute += RemoveGold3Gold_Execute;
-
-            var removeGold5Gold = new SimpleAction(this, "Remove5Gold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-5з"
-            };
-            removeGold5Gold.Execute += RemoveGold5Gold_Execute;
-
-            var removeGold10Gold = new SimpleAction(this, "RemoveTenGold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-10з"
-            };
-            removeGold10Gold.Execute += RemoveGold10Gold_Execute;
-
-            var removeGold100Gold = new SimpleAction(this, "Remove100Gold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-100з"
-            };
-            removeGold100Gold.Execute += RemoveGold100Gold_Execute;
-
-            var removeGold250Gold = new SimpleAction(this, "Remove250Gold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-250з"
-            };
-            removeGold250Gold.Execute += RemoveGold250Gold_Execute;
-
-            var removeGold500Gold = new SimpleAction(this, "Remove500Gold", ActionCategories.RemoveGoldCategory)
-            {
-                Caption = "-500з"
-            };
-            removeGold500Gold.Execute += RemoveGold500Gold_Execute;
-
-
-            var simpleTransferGold = new SimpleAction(this, "SimpleTransferGold", ActionCategories.SimpleTransferGoldCategory)
-            {
-                Caption = "Между своими"
-            };
-            simpleTransferGold.Execute += SimpleTransferGold_Execute;
-
-            var sendGold = new SimpleAction(this, "SendGold", ActionCategories.SimpleTransferGoldCategory)
-            {
-                Caption = "Отправить"
-            };
-            sendGold.Execute += SendGold_Execute;
+            CreateSimpleAction("Между своими", "SimpleTransferGold", ActionCategories.SimpleTransferGoldCategory, SimpleTransferGold_Execute);
+            CreateSimpleAction("Отправить", "SendGold", ActionCategories.SimpleTransferGoldCategory, SendGold_Execute);
             #endregion
         }
         #region Events
@@ -167,6 +86,17 @@ namespace ZeeKer.DndTracker.Module.Controllers
 
 
         #region Methods
+
+        private SimpleAction CreateSimpleAction(string id, string caption, string category, SimpleActionExecuteEventHandler handler)
+        {
+            var action = new SimpleAction(this, id, category)
+            {
+                Caption = caption
+            };
+            action.Execute += handler;
+            return action;
+        }
+
         private void ExecuteSimpleOperation(Guid? campainId, decimal coins, StorageOperationType type, bool fastOperation = false)
         => useCase
             .Execute(
