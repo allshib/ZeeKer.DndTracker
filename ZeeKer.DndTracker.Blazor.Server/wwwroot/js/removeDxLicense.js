3ad3df2bb43ca3207d969adbe5fee2e66bfbebac
Alex Shibalkin <shibalkun@ya.ru>
2024-06-18 12:08:31 +0500

    Исправлены ошибки переезда #DND-52

--- ZeeKer.DndTracker.Blazor.Server/wwwroot/js/removeDxLicense.js
+++ ZeeKer.DndTracker.Blazor.Server/wwwroot/js/removeDxLicense.js
@@ -1,24 +1,14 @@
 window.clearDxLicenseTag = () => {
     const dxLicenseTag = document.querySelector('dx-license');
     if (dxLicenseTag) {
-        dxLicenseTag.setAttribute('version', '');
+        const innerDiv = dxLicenseTag.querySelectorAll('div');
+        if (innerDiv) {
+            innerDiv.forEach(di => {
+                di.click();
+            })
+        }
     }
+    
+    
 
-    const innerDiv = dxLicenseTag.querySelectorAll('div');
-
-    innerDiv.forEach(di => {
-        di.click();
-    })
-
-    //if (innerDiv) {
-    //    const spans = innerDiv.querySelectorAll('span');
-    //    console.log(spans);
-    //    spans.forEach(span => {
-    //        span.style = 'none !important';
-    //    });
-
-    //    const link = innerDiv.querySelector('a');
-    //    console.log(link);
-        
-    //}
 };
